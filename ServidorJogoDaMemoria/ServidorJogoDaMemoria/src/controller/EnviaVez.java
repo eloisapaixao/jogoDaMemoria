93d154d2451f688993a6f6ff69c1a03b20fe9a3a
BiBee08 <balvesbatista67@gmail.com>
2022-12-11 18:12:45 -0300

    Atualização 11/12/2022
    agora sim funcional

--- ServidorJogoDaMemoria/ServidorJogoDaMemoria/src/controller/EnviaVez.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-public class EnviaVez extends Comunicado {
-    public final int VEZ;
-    public EnviaVez(int VEZ)
-    {
-        this.VEZ = VEZ;
-    }
-}
--- ServidorJogoDaMemoria/ServidorJogoDaMemoria/src/controller/PedidoDeSair.java
+++
@@ -1,3 +0,0 @@
-package controller;
-
-public class PedidoDeSair extends Comunicado {  }
